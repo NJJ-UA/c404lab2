cecce92250de985adf48bed3e286096981008c21
Nidedaye <jonathan.nijingqiao@gmail.com>
2016-09-14 14:24:19 -0600

    finish by q1

---
+++ lab2.py
@@ -0,0 +1,19 @@
+import socket
+
+client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+client.connect(('www.google.ca',80))
+
+http = "GET / HTTP/1.0\r\n\r\n"
+
+client.sendall(http)
+
+msg=""
+while True:
+      part = client.recv(1024)
+      if part:
+            msg +=part
+      else:
+            break
+
+print msg
